2847433c8be3767ea71fc200227d2498893e8b56
Riccardo Persello <riccardo.persello@icloud.com>
2023-05-25 18:06:37 +0200

    First unreliable prototype of real-time compilation.

--- Serifian/Views/PDFView.swift
+++ Serifian/Views/PDFView.swift
@@ -10,6 +10,7 @@ import PDFKit
 
 struct PDFView {
     let document: PDFDocument
+    @State private var currentlyActiveView: PDFKit.PDFView?
 }
 
 #if os(macOS)
@@ -27,39 +28,56 @@ extension PDFView: NSViewRepresentable {
 
         container.addSubview(pdfView)
 
+        DispatchQueue.main.async {
+            self.currentlyActiveView = pdfView
+        }
+
         return container
     }
 
-
-
     func updateNSView(_ nsView: NSView, context: Context) {
-        let oldPdfView = nsView.subviews.first! as! PDFKit.PDFView
-        let oldScrollView = (oldPdfView.subviews.first! as! NSScrollView)
+        // Do not accept updates when we are in a state with more than one active view.
+        guard let currentlyActiveView else {
+            return
+        }
 
+        guard self.document != currentlyActiveView.document else {
+            return
+        }
+
+        // Get old scroll view.
+        let oldScrollView = (currentlyActiveView.subviews.first! as! NSScrollView)
+
+        // Create a new PDFView, that will be hidden under the older ones.
         let newPdfView = PDFKit.PDFView(frame: nsView.bounds)
         newPdfView.document = self.document
         newPdfView.autoresizingMask = [.height, .width]
 
-        nsView.addSubview(newPdfView, positioned: .below, relativeTo: nsView.subviews.first!)
+        nsView.addSubview(newPdfView, positioned: .below, relativeTo: currentlyActiveView)
 
-        nsView.layout()
+        // We have two (or more?) overlaid `PDFView`s now.
+        DispatchQueue.main.async {
+            self.currentlyActiveView = nil
+        }
 
-
-        Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false) { timer in
+        // After the new view rendered, we restore the zoom and scroll state in the new one.
+        Timer.scheduledTimer(withTimeInterval: 0.3, repeats: false) { timer in
 
             // TODO: Intelligently delay this operation.
-
             let newScrollView = (newPdfView.subviews.first! as! NSScrollView)
 
             let oldRect = oldScrollView.contentView.bounds
             newScrollView.setMagnification(oldScrollView.magnification, centeredAt: .zero)
             newScrollView.contentView.scrollToVisible(oldRect)
 
-
             for view in nsView.subviews {
                 if view != newPdfView {
                     view.removeFromSuperview()
                 }
+            }
+
+            DispatchQueue.main.async {
+                self.currentlyActiveView = newPdfView
             }
         }
     }
