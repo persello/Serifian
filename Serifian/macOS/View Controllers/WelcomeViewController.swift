cc00cad6fcd0cfea3234e0c0ede1778a86aaaca3
Riccardo Persello <riccardo.persello@icloud.com>
2023-10-16 11:34:51 +0200

    Started binding welcome outline view to recent files.

--- Serifian/macOS/View Controllers/WelcomeViewController.swift
+++ Serifian/macOS/View Controllers/WelcomeViewController.swift
@@ -13,26 +13,42 @@ class WelcomeViewController: NSViewController {
     @IBOutlet weak var chooseFromTemplateButton: NSButton!
     
     @IBOutlet weak var recentFilesOutlineView: NSOutlineView!
+    @IBAction func recentFilesDoubleAction(_ sender: NSOutlineView) {
+//        sender.click
+        print("Selected \(sender.child(sender.selectedRow, ofItem: nil))")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do view setup here.
     }
 }
 
 extension WelcomeViewController: NSOutlineViewDataSource {
     func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
-        return item == nil ? 5 : 0
+        return NSDocumentController.shared.recentDocumentURLs.count
     }
     
     func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
-        return ()
+        return NSDocumentController.shared.recentDocumentURLs[index]
     }
     
+    func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
+        guard let url = item as? URL else { return nil }
+        let view = RecentFileCell()
+        view.title.stringValue = url.deletingPathExtension().lastPathComponent
+        view.path.stringValue = url.deletingLastPathComponent().path()
+        
+        let imageURL = url.appending(path: "cover.jpeg")
+        if let data = try? Data(contentsOf: imageURL) {
+            let image = NSImage(data: data)
+            view.imageView?.image = image
+        }
+        
+        return view
+    }
     
-}
-
-extension WelcomeViewController: NSOutlineViewDelegate {
-
 }
 
 #Preview("Welcome View Controller") {
